e4a6d27e280d414c0bdc60764b6b660497139ef2
mohammedsayis <mohammedsayis@gmail.com>
2026-04-19 18:42:33 +0530

    updated hero page

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -6,7 +6,7 @@ function Hero() {
       <div className='leftSide'>
         <div>
           <h1 className='name'>Hello, I'm Mohammed Sayis CH </h1>
-        <h3 className='position'>MERN Stack Developer 👨🏻‍💻</h3>
+          <h3 className='position'>MERN Stack Developer 👨🏻‍💻</h3>
         <div className='socialMedia'>
           <a href="mailto:mohammedsayis@gmail.com" > 📩 mohammedsayis@gmail.com</a>
           <a href='https://www.linkedin.com/in/mohammed-sayis-ch-88b170226/'> ℹ️ Mohammed Sayis ch</a>
